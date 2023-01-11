b193f5dc0be5a5deeeaa1e4dc249c8a020829468
syakaidane <121926213+syakaidane@users.noreply.github.com>
2023-01-11 05:13:06 +0000

    commit2

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -6,28 +6,43 @@ class ListsController < ApplicationController
   
   # 以下を追加
   def create
-    # １.&2. データを受け取り新規登録するためのインスタンス作成
-    list = List.new(list_params)
-    # 3. データをデータベースに保存するためのsaveメソッド実行
-    list.save
-    # 4. トップ画面へリダイレクト
-    redirect_to '/top'
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list.id)
+    else
+      render :new
+    end
   end
   
 
   def index
+    @lists = List.all
   end
 
   def show
+    @list = List.find(params[:id]) 
   end
 
   def edit
+    @list = List.find(params[:id])
   end
-
+  
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)  
+  end
+  
+  def destroy
+    list = List.find(params[:id])  # データ（レコード）を1件取得
+    list.destroy  # データ（レコード）を削除
+    redirect_to '/lists'  # 投稿一覧画面へリダイレクト  
+  end
+  
 private
   # ストロングパラメータ
   def list_params
-    params.require(:list).permit(:title, :body)
+    params.require(:list).permit(:title, :body, :image)
   end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  get 'lists/new'
-  post 'lists' => 'lists#create'   #ここを追記します
-  get 'lists/index'
-  get 'lists/show'
-  get 'lists/edit'
-  get '/top' => 'homes#top'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  get 'top' => 'homes#top'
+  resources :lists
 end
